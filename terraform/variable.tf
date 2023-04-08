9cc302e1ff8735f697761f0176c1d6199ae40552
Muhammed <muhammed@sendme.ng>
2023-04-08 05:23:08 +0100

    feat: setup terraform

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,77 +1,24 @@
-variable "aws_region" {
-    default = "eu-central-1"
+locals {
+  data_lake_bucket = "dtc_data_lake"
 }
 
-variable "vpc_cidr_block" {
-    description = "CIDR block for VPC"
-    type        = string
-    default     = "10.0.0.0/16"
+variable "project" {
+  description = "Your GCP Project ID"
 }
 
-variable "subnet_count" {
-    description = "Number of subnets"
-    type        = map(number)
-    default = {
-        public = 1,
-        private = 2
-    }
+variable "region" {
+  description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
+  default = "europe-west6"
+  type = string
 }
 
-variable "settings" {
-    description = "Configuration settings"
-    type        = map(any)
-    default = {
-        "database" = {
-            allocated_storage   = 10
-            engine              = "postgres"
-            engine_version      = "13.7"
-            instance_class      = "db.t3.micro"
-            db_name             = "nyc_taxy_data"
-            skip_final_snapshot = true
-        },
-        "web_app" = {
-            count           = 1
-            instance_type   = "t2.micro"
-        }
-    }
+variable "storage_class" {
+  description = "Storage class type for your bucket. Check official docs for more info."
+  default = "STANDARD"
 }
 
-variable "public_subnet_cidr_blocks" {
-    description = "Availability CIDR blocks for public subnets"
-    type        = list(string)
-    default = [
-        "10.0.1.0/24",
-        "10.0.2.0/24",
-        "10.0.3.0/24",
-        "10.0.4.0/24"
-    ]
-}
-
-variable "private_subnet_cidr_blocks" {
-    description = "Available CIDR blocks for private subnets"
-    type        = list(string)
-    default = [
-        "10.0.101.0/24",
-        "10.0.102.0/24",
-        "10.0.103.0/24",
-        "10.0.104.0/24"
-    ]
-}
-
-variable "my_ip" {
-    description = "Your IP address"
-    type        = string
-    sensitive   = true
-}
-
-variable "db_username" {
-    description = "Database master user"
-    type        = string
-    sensitive   = true
-}
-
-variable "db_password" {
-    description = "Database master user password"
-    type        = string
-    sensitive   = true
+variable "BQ_DATASET" {
+  description = "BigQuery Dataset that raw data (from GCS) will be written to"
+  type = string
+  default = "trips_data_all"
 }
