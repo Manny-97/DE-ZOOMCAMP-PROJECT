cde762d45b8744d08080d711fa30f515fe62de29
Muhammed <muhammed@sendme.ng>
2023-05-01 08:04:40 +0100

    feat: add option to install new executable path using service

--- data-extraction/scraper.py
+++ data-extraction/scraper.py
@@ -1,27 +1,30 @@
-import pandas as pd
-from bs4 import BeautifulSoup
 import time
 
+import pandas as pd
+from bs4 import BeautifulSoup
 from selenium import webdriver
 
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
 
 
 def get_html(page: int):
-    """This method uses selenium to call the web page, and then uses 
+    """This method uses selenium to call the web page, and then uses
     beautifulsoup to extract the statistics table from the html of the page.
     Args:
     page (int): The page number for which we want to extract data.
     """
-    print('Opening Chrome.......')
-    path_to_exec = 'D:/KTM project/Motogp/chromedriver_win32/chromedriver.exe'
+    print("Opening Chrome.......")
+    path_to_exec = "D:/KTM project/Motogp/chromedriver_win32/chromedriver.exe"
+    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver = webdriver.Chrome(executable_path=path_to_exec)
-    baseurl = 'https://www.motogp.com/en/statistics/gp-race-winners/All-seasons/'
-    driver.get(f'{baseurl}All-circuits/All-classes/All-countries/?page={page}')
+    baseurl = "https://www.motogp.com/en/statistics/gp-race-winners/All-seasons/"
+    driver.get(f"{baseurl}All-circuits/All-classes/All-countries/?page={page}")
     time.sleep(10)
     page_source = driver.page_source
-    soup = BeautifulSoup(page_source, 'html.parser')
-   
-    table = soup.find_all('tr', class_="table_row")
+    soup = BeautifulSoup(page_source, "html.parser")
+
+    table = soup.find_all("tr", class_="table_row")
     return table
 
 
@@ -32,45 +35,58 @@ def extract_rider_info(table):
     """
     reviews = []
     for t in table:
-        p=1
+        p = 1
         try:
-            name=t.find("span",{"class":"surname qa_table_rider_surname"}).text.replace('\n',"")
+            name = t.find(
+                "span", {"class": "surname qa_table_rider_surname"}
+            ).text.replace("\n", "")
         except:
             name = None
         try:
-            season=t.find("span",{"class":"font-weight-bold"}).text.replace('\n',"")
+            season = t.find("span", {"class": "font-weight-bold"}).text.replace(
+                "\n", ""
+            )
         except:
             season = None
         try:
-            country=t.find("span",{"class":"country"}).text.replace('\n',"")
+            country = t.find("span", {"class": "country"}).text.replace("\n", "")
         except:
             country = None
         try:
-            circuit=t.find("span",{"class":"pl-1 d-block d-sm-none"}).text.replace('\n',"")
+            circuit = t.find("span", {"class": "pl-1 d-block d-sm-none"}).text.replace(
+                "\n", ""
+            )
         except:
             circuit = None
         try:
-            const_class = "table_item constructor qa_table_constructor d-none d-sm-table-cell"
-            constructor=t.find("td",{"class":const_class}).text.replace('\n',"")
+            const_class = (
+                "table_item constructor qa_table_constructor d-none d-sm-table-cell"
+            )
+            constructor = t.find("td", {"class": const_class}).text.replace("\n", "")
         except:
             constructor = None
         try:
-            ride_class = "table_item category qa_table_category d-table-cell text-right pr-8"
-            ride_class=t.find("td",{"class":ride_class}).text.replace('\n',"")
+            ride_class = (
+                "table_item category qa_table_category d-table-cell text-right pr-8"
+            )
+            ride_class = t.find("td", {"class": ride_class}).text.replace("\n", "")
         except:
             ride_class = None
         rider_name = {
-            "name": name, "season": season, "country": country, 
-            "circuit": circuit, "constructor": constructor, 
-            "ride_class": ride_class
-            }
-        
+            "name": name,
+            "season": season,
+            "country": country,
+            "circuit": circuit,
+            "constructor": constructor,
+            "ride_class": ride_class,
+        }
+
         reviews.append(rider_name)
     return reviews
 
 
 def convert_to_csv(last_page: int):
-    """This function calls the other previous functions from page 1 
+    """This function calls the other previous functions from page 1
     to the last page, and dump the reult into a csv file
     Args:
     last_page: the last page you want to extract data up to.
@@ -89,9 +105,9 @@ def convert_to_csv(last_page: int):
 
     df = pd.DataFrame(full_data)
 
-
     df.to_csv("data/raw/motogp.csv", index=False)
     print(df.tail())
+
 
 if __name__ == "__main__":
     convert_to_csv(last_page=169)
