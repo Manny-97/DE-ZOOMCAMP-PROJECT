b5175637678e1fef568f4f5e26dd48a90254a3e1
Muhammed <muhammed@sendme.ng>
2023-05-04 17:58:24 +0100

    feat: add access key var

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,16 @@
 provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
   region = var.aws_region
 }
 
 resource "aws_s3_bucket" "my-s3-bucket" {
   bucket_prefix = var.bucket_prefix
   acl = var.acl
-  
+
    versioning {
     enabled = var.versioning
   }
-  
+
   tags = var.tags
 }
